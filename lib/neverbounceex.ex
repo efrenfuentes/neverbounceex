613c22c9c4e18cce3c142de34aebe767de87bf98
Efrén Fuentes <efrenfuentes@gmail.com>
2017-08-10 13:50:09 -0400

    Init project

---
+++ lib/neverbounceex.ex
@@ -0,0 +1,18 @@
+defmodule Neverbounceex do
+  @moduledoc """
+  Documentation for Neverbounceex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Neverbounceex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/neverbounceex_test.exs
@@ -0,0 +1,8 @@
+defmodule NeverbounceexTest do
+  use ExUnit.Case
+  doctest Neverbounceex
+
+  test "greets the world" do
+    assert Neverbounceex.hello() == :world
+  end
+end
