8ca3c2f0f0ebada68c5949a4b00f1fc97c3fd00e
Efrén Fuentes <efrenfuentes@gmail.com>
2017-08-12 00:59:31 -0400

    add new dependencies httposion for http request, credo for analyze code

--- mix.exs
+++ mix.exs
@@ -14,16 +14,17 @@ defmodule NeverBounceEx.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {NeverBounceEx.Application, []}
+      extra_applications: [:logger, :httpoison],
+      mod: {NeverBounceEx, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:httpoison, "~> 0.12"},
+      {:poison, "~> 3.1"},
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
     ]
   end
 end
